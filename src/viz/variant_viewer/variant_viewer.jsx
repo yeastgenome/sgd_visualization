5a5a767b1b91e87bfd2e4279def89b63ff4f6362
Travis Sheppard <tshepp@stanford.edu>
2015-06-17 10:53:57 -0700

    example data

---
+++ src/viz/variant_viewer/variant_viewer.jsx
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+"use strict";
+var d3 = require("d3");
+var React = require("react");
+var _ = require("underscore");
+
+var VariantViewer = React.createClass({
+	render: function () {
+		return <h1>VariantViewer</h1>;
+	}
+});
+
+module.exports = VariantViewer;
