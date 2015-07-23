67513655ccbd6da80b55fab181cf28e063d4157c
Travis Sheppard <tshepp@stanford.edu>
2015-07-23 10:51:14 -0700

    fix test

--- test/variant_viewer_test.js
+++ test/variant_viewer_test.js
@@ -5,10 +5,32 @@ var React = require("react");
 require("node-jsx").install({ harmony: true });
 
 var VariantViewer = require("../src/sgd_visualization.jsx").VariantViewer;
+var FeatureViewerStore = require("../src/store/feature_viewer_store.jsx");
 var exampleData = require("../examples/variant_viewer/example_alignment_data.json");
 
 describe("VariantViewer", function(){
 	it("should render to a viz with classes 'sgd-viz' and 'variant-viewer'", function(){
+
+		// init store
+		var featureTrackData = {
+				id: "variantViewer",
+				position: {
+					chromStart: exampleData.chromStart,
+					chromEnd: exampleData.chromEnd
+				},
+				features: [
+					{
+						chrom: "chriii", // TEMP
+						chromStart:  exampleData.chromStart,
+						chromEnd: exampleData.chromEnd,
+						strand: "+",
+						blockSizes: exampleData.blockSizes,
+						blockStarts: exampleData.blockStarts
+					}
+				]
+			};
+		var _store = new FeatureViewerStore();
+		_store.addFeatureTrack(featureTrackData);
 
 		var markup = React.renderToStaticMarkup(React.createElement(VariantViewer, {
 			alignedDnaSequences: exampleData.aligned_dna_sequences,
@@ -20,6 +42,7 @@ describe("VariantViewer", function(){
 			dnaLength: exampleData.dna_length,
 			proteinLength: exampleData.protein_length,
 			strand: exampleData.strand,
+			store: _store
 		}));
 		assert.equal(markup.match('class="sgd-viz variant-viewer') !== null, true);
 		assert.equal(markup.match(/<div/).index, 0);
