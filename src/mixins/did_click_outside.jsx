9e04a8a96f13e44b4917faa9851152bae53042d8
Sagar <sagar.jhaa@gmail.com>
2019-07-25 18:09:35 -0700

    Update to R15

--- src/mixins/did_click_outside.jsx
+++ src/mixins/did_click_outside.jsx
@@ -3,8 +3,26 @@
 /*
 	Assumes that component has method called didClickOutside, which handles being clicked outside
 */
+import React,{Component} from 'react';
+function DidClickOutside(WrappedComponent){
+	return class extends Component{
+		componentDidMount(){
+			document.addEventListener("click",this._handleClick);
+		}
 
-var DidClickOutside = {
+		_handleClick(){
+			if (this.isMounted() && this.didClickOutside) {
+				this.didClickOutside();
+			}
+		}
+
+		render(){
+			return(<WrappedComponent />);
+		}
+	}
+}
+
+export default DidClickOutside = {
 	// add event listener to document to dismiss when clicking
 	componentDidMount: function () {
 		document.addEventListener("click", () => {
@@ -15,4 +33,3 @@ var DidClickOutside = {
 	},
 };
 
-export default DidClickOutside;
