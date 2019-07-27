58948f9dcf0b78c6d60f02821de2ec637f5b1118
Sagar <sagar.jhaa@gmail.com>
2019-07-26 17:01:10 -0700

    Update canvas_ratio mixin

--- src/mixins/calculate_canvas_ratio.jsx
+++ src/mixins/calculate_canvas_ratio.jsx
@@ -2,20 +2,33 @@
 
 // calculate drawing ratio to solve blurry high DPI canvas issue
 // cb passed to cb for this.setState
-var CalculateCanvasRatio = {
-	calculateCanvasRatio: function (cb) {
-		// query device pixel ratio
-		var ctx = this.refs.canvas.getContext("2d");
-		var devicePixelRatio = window.devicePixelRatio || 1;
-		var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+import React,{Component} from 'react';
+function CalculateCanvasRatio(WrappedComponent){
+	return class extends Component{
+		constructor(props){
+			super(props);
+		}
+
+		calculateCanvasRatio(component){
+			// query device pixel ratio
+			var ctx = component.refs.canvas.getContext("2d");
+			var devicePixelRatio = window.devicePixelRatio || 1;
+			var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
 			ctx.mozBackingStorePixelRatio ||
 			ctx.msBackingStorePixelRatio ||
 			ctx.oBackingStorePixelRatio ||
 			ctx.backingStorePixelRatio || 1;
 
-	    var _canvasRatio = devicePixelRatio / backingStoreRatio;
-	    this.setState({ canvasRatio: _canvasRatio }, cb);
-	},
-};
+			var _canvasRatio = devicePixelRatio / backingStoreRatio;
+			return _canvasRatio;
+			// this.setState({ canvasRatio: _canvasRatio }, cb);
+		}
+
+		render(){
+			return(<WrappedComponent {...this.props} calculateCanvasRatio={this.calculateCanvasRatio}/>)
+		}
+	}
+}
 
 export default CalculateCanvasRatio;
+
