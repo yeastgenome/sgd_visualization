a5fac526bceab3d04c3ef0f15ab6673bc34f0453
Sagar <sagar.jhaa@gmail.com>
2019-07-29 11:59:31 -0700

    Add webpack to build process

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: 'development',
+  output: {
+    filename: "./sgd_visualization.js",
+    path: path.join(__dirname, "build")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  stats: {
+    colors: true,
+    modules: true,
+    reasons: true,
+    errorDetails: false
+  }
+}
