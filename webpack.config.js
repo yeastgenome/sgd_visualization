4c8aa2f570fee5f3e6e62789b4d5c2fa874cfb6f
Sagar <sagar.jhaa@gmail.com>
2019-07-31 10:43:56 -0700

    Update node v10

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: "./index.js",
     path: path.join(__dirname, "lib"),
