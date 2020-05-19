3be0be4593863c39c9c908d2bbe180aa365764a9
Shuai Weng <sweng@stanford.edu>
2020-05-19 12:16:51 -0700

    testing

--- src/viz/variant_viewer/alignment_model.jsx
+++ src/viz/variant_viewer/alignment_model.jsx
@@ -47,7 +47,7 @@ module.exports = class AlignmentModel {
 	}
 
 	// from start and end of aligned sequence, return reference coordinates (currently always S288C)
-	getReferenceCoordinatesFromAlignedCoordinates (alignedStart, alignedEnd, isProtein) {
+        getReferenceCoordinatesFromAlignedCoordinates (alignedStart, alignedEnd, isProtein, alignedDnaStart, alignedDnaEnd) {
 		var _attr = this.attributes;
 		var _seqs = isProtein ? _attr.alignedProteinSequences : _attr.alignedDnaSequences;
 		var referenceSequence = _.findWhere(_seqs, { name: _attr.referenceName }).sequence;
