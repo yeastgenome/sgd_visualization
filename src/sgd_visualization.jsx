e779b3cb64458c5e2e06069d857311c3e4d3b2f2
Shuai Weng <sweng@stanford.edu>
2020-06-29 11:57:40 -0700

    get rid of deepScan warning

--- src/sgd_visualization.jsx
+++ src/sgd_visualization.jsx
@@ -1,7 +1,6 @@
 "use strict";
 
 import d3 from 'd3';
-import React from 'react';
 import ProteinViewer from '../src/viz/protein_viewer.jsx';
 import VariantViewer from '../src/viz/variant_viewer/variant_viewer.jsx';
 
